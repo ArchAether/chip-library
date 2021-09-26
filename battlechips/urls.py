01dc0456d49c815d962678b0962ee89de6e8a44d
Stephen <archaether@gmail.com>
2021-09-26 09:39:35 -0400

    Implemented more views
    Implemented view that return all battlechips, return chips by ID, return chips by element, return chips by words in description

--- battlechips/urls.py
+++ battlechips/urls.py
@@ -1,8 +1,12 @@
+from os import name
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('mmbn6/', views.mmbn6_all, name='mmbn6_all'),
-    # path('', views.index, name='index'),
+    path('<int:id_number>/', views.mmbn6_by_id, name='by_id'),
+    path('<str:name>/', views.mmbn_by_name, name='by_name'),
+    path('elem/<str:elem>/', views.mmbn6_by_elem, name="by_elem"),
+    path('desc/<str:description>/', views.mmbn6_by_description, name = "by_location"),
+    path('', views.mmbn6_all, name='all'),
 ]
--- battlechips/views.py
+++ battlechips/views.py
@@ -1,3 +1,4 @@
+import battlechips
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -6,10 +7,46 @@ import csv
 
 # Create your views here.
 def mmbn6_all(request):
+    battlechips = get_battleChips()
+    return JsonResponse(battlechips, safe=False)
+
+def mmbn_by_name(request, name):
+    battlechips = get_battleChips()
+    results = []
+    for chip in battlechips:
+        if name.lower() in chip.get('name').lower():
+            results.append(chip)
+    return JsonResponse(results, safe=False)
+
+def mmbn6_by_id(request, id_number):
+    battlechips = get_battleChips()
+    result = None
+    for chip in battlechips:
+        if chip.get('number') == id_number:
+            result = chip
+    return JsonResponse(result)
+
+def mmbn6_by_elem(request, elem):
+    battlechips = get_battleChips()
+    results = []
+    for chip in battlechips:
+        if chip.get('type') == elem.lower():
+            results.append(chip)
+    return JsonResponse(results, safe=False)
+
+def mmbn6_by_description(request, description):
+    battlechips = get_battleChips()
+    result = []
+    for chip in battlechips:
+        if description.lower() in chip.get('description').lower():
+            result.append(chip)
+    return JsonResponse(result, safe=False)
+
+def get_battleChips():
     battlechips = []
-    with open('battlechips/static/csvs/StandardBattleChips.csv', 'r', encoding='utf-8') as f:
+    with open('battlechips/static/csvs/MMBN6-BattleChips.csv', 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         next(reader)
         for row in reader:
-            battlechips.append({'number': int(row[0]), 'name': row[2], 'damage': row[4], 'description': row[5], 'location': row[6]})
-    return JsonResponse(battlechips, safe=False)
+            battlechips.append({'number': int(row[0]), 'name': row[2], 'type': row[3], 'damage': row[4], 'description': row[5], 'location': row[6]})
+    return battlechips
